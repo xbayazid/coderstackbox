1289ae7e809efb7d476a5ef518955a9a2ec7d2f9
xbayazid <bayazid9006@gmail.com>
2023-01-19 23:38:40 +0600

    updated

--- src/Pages/catagories/Catagories.js
+++ src/Pages/catagories/Catagories.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Right from '../Home/Doc/Right';
 import Leftcategory from './Leftcategory';
 
 const Catagories = () => {
     return (
         <div className='text-white'>
            <Leftcategory></Leftcategory>
-            <Right></Right>
         </div>
     );
 };
