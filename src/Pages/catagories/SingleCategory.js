faba73e1cf4fedcbf2116c515ab493dbaf6206b3
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2023-01-30 20:07:12 +0600

    Merge branch 'main' of https://github.com/xbayazid/coderstackbox into noman

--- src/Pages/catagories/SingleCategory.js
+++ src/Pages/catagories/SingleCategory.js
@@ -11,7 +11,7 @@ const SingleCategory = () => {
       <div
         className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4${layout.sectionCol}`}
       >
-        {projects.map((p) => (
+        {projects?.map((p) => (
           <div>
             <h2 className="text-lg text-white font-bold m-2">
               Example for{" "}
