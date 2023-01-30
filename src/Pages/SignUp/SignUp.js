885734d4715d26d8bb5c49bfd17635490b53de3c
Chayon <chayon15-3633@diu.edu.bd>
2023-01-30 22:08:00 +0600

    react-helmet

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import { GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth';
 import React, { useContext, useState } from 'react';
+import { Helmet } from 'react-helmet';
 import { toast } from 'react-hot-toast';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
@@ -67,50 +68,56 @@ const SingUp = () => {
       });
   };
   return (
-    <div className='grid gap-4 md:grid-cols-1 lg:grid-cols-2'>
-      <div className={`${layout.sectionCol}`}>
-        <img className='extra-img w-4/5' src="https://i.ibb.co/S3ScgR8/118046-lf20-oahmox5rjson.gif" alt="" />
-      </div>
-      <div className='extra p-2'>
-        <div className='text-white text-center xl:mt-16'>
-          <div className='flex justify-around w-56 mx-auto'>
-            <div>
-              <h3 className='uppercase mb-3 -ml-10 authentication-title'>Register</h3>
-            </div>
-            <div>
-              <Link to='/login'>
-                <button className="btn btn-xs ml-10">Login</button>
-              </Link>
-            </div>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>CodersStackBox - SignUp</title>
+      </Helmet>
+      <div className='grid gap-4 md:grid-cols-1 lg:grid-cols-2'>
+        <div className={`${layout.sectionCol}`}>
+          <img className='extra-img w-4/5' src="https://i.ibb.co/S3ScgR8/118046-lf20-oahmox5rjson.gif" alt="" />
+        </div>
+        <div className='extra p-2'>
+          <div className='text-white text-center xl:mt-16'>
+            <div className='flex justify-around w-56 mx-auto'>
+              <div>
+                <h3 className='uppercase mb-3 -ml-10 authentication-title'>Register</h3>
+              </div>
+              <div>
+                <Link to='/login'>
+                  <button className="btn btn-xs ml-10">Login</button>
+                </Link>
+              </div>
 
             </div>
             <div className='mt-6'>
-                <form onSubmit={handleSubmit}>
+              <form onSubmit={handleSubmit}>
                 <div className='mb-5'>
-                <input type="name" name='name' placeholder="Full Name" className="input input-bordered w-full max-w-xs text-black" required/>
+                  <input type="name" name='name' placeholder="Full Name" className="input input-bordered w-full max-w-xs text-black" required />
                 </div>
                 <div>
-                <input type="email" name='email' placeholder="Email Address" className="input input-bordered w-full max-w-xs text-black" required />
+                  <input type="email" name='email' placeholder="Email Address" className="input input-bordered w-full max-w-xs text-black" required />
                 </div>
                 <div className='my-5'>
-                <input type="password" name='password' placeholder="Password" className="input input-bordered w-full max-w-xs text-black" required />
+                  <input type="password" name='password' placeholder="Password" className="input input-bordered w-full max-w-xs text-black" required />
                 </div>
                 <div>
-                    <button className='bg-black px-32 py-3 text-white rounded-md' type='submit'>Register</button>
+                  <button className='bg-black px-32 py-3 text-white rounded-md' type='submit'>Register</button>
                 </div>
                 <div className="divider w-52 mx-auto">OR</div>
-                <div className='mx-auto p-3 rounded-md bg-gradient-to-r from-accent to-secondary' style={{width: '317px'}}>
-                    <button onClick={handleGoogleSignIn}>Register with Google</button>
+                <div className='mx-auto p-3 rounded-md bg-gradient-to-r from-accent to-secondary' style={{ width: '317px' }}>
+                  <button onClick={handleGoogleSignIn}>Register with Google</button>
                 </div>
-                <div className='mx-auto p-3 rounded-md mt-4 bg-gradient-to-r from-accent to-secondary' style={{width: '317px'}}>
-                    <button onClick={hangleGithubSingIn}>Register with GitHub</button>
+                <div className='mx-auto p-3 rounded-md mt-4 bg-gradient-to-r from-accent to-secondary' style={{ width: '317px' }}>
+                  <button onClick={hangleGithubSingIn}>Register with GitHub</button>
                 </div>
-                </form>
+              </form>
             </div>
-            </div>
-            </div>            
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SingUp;
--- src/Pages/UserDashboard/MyProjects/MyProjects.js
+++ src/Pages/UserDashboard/MyProjects/MyProjects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { FaHome, FaUsers, FaSwatchbook, FaUserCircle, FaRegEnvelope, FaSignOutAlt, FaExclamationTriangle, FaBars, FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Button from '../../../components/Buttons/Button';
@@ -6,45 +7,49 @@ import Button from '../../../components/Buttons/Button';
 const MyProjects = () => {
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>CodersStackBox - MyProjects</title>
+            </Helmet>
             <div className="flex text-white">
-            <div className='hidden lg:block md:w-3/12 h-[100vh] bg-blue-green-gradient z-50'>
-                <div className='absolute sidebar-menu ml-3 text-white text-2xl'>
-                    <ul className='mt-20'>
-                    <li>
-                            <Link to="/userProfile"> <FaUserCircle className='sidebar-icon'></FaUserCircle> <span>Profile</span> </Link>
-                        </li>
-                        <li>
-                            <Link to="/myProjects"> <FaSwatchbook className='sidebar-icon'></FaSwatchbook> <span>My Projects</span> </Link>
-                        </li>
-                        
-                    </ul>
+                <div className='hidden lg:block md:w-3/12 h-[100vh] bg-blue-green-gradient z-50'>
+                    <div className='absolute sidebar-menu ml-3 text-white text-2xl'>
+                        <ul className='mt-20'>
+                            <li>
+                                <Link to="/userProfile"> <FaUserCircle className='sidebar-icon'></FaUserCircle> <span>Profile</span> </Link>
+                            </li>
+                            <li>
+                                <Link to="/myProjects"> <FaSwatchbook className='sidebar-icon'></FaSwatchbook> <span>My Projects</span> </Link>
+                            </li>
 
-                    <div>
-                        <FaSignOutAlt className=''></FaSignOutAlt>
+                        </ul>
+
+                        <div>
+                            <FaSignOutAlt className=''></FaSignOutAlt>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='main-content w-full'>
+                <div className='main-content w-full'>
 
-                <header className='bg-dark-1 p-4'>
-                    <div className='flex justify-between items-center'>
-                        <div className='header-title text-3xl flex gap-x-5 items-center'>
-                            <label htmlFor=""><FaBars></FaBars></label>Overview
+                    <header className='bg-dark-1 p-4'>
+                        <div className='flex justify-between items-center'>
+                            <div className='header-title text-3xl flex gap-x-5 items-center'>
+                                <label htmlFor=""><FaBars></FaBars></label>Overview
+                            </div>
+                            <div className='user-wrapper text-3xl '>
+                                <FaUserCircle></FaUserCircle>
+                            </div>
                         </div>
-                        <div className='user-wrapper text-3xl '>
-                            <FaUserCircle></FaUserCircle>
-                        </div>
-                    </div>
-                </header>
+                    </header>
 
-                <main className='py-5 px-10'>
-                {/* we will load the username here from email */}
-                    <h2 className='text-3xl mb-5 others'>My project</h2> 
-                    
-                </main>
+                    <main className='py-5 px-10'>
+                        {/* we will load the username here from email */}
+                        <h2 className='text-3xl mb-5 others'>My project</h2>
+
+                    </main>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
