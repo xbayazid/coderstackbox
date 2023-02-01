b46e0e6dd8a294eb90e5b4a8685d637a2fd4329a
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-02 01:00:08 +0600

    header -code editor page

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+import { FaEnvelope, FaFileInvoice, FaHome } from "react-icons/fa";
 
 export const sidebarMenuItem = [
   {
@@ -136,4 +137,23 @@ export const bestDevs = [
     title: "Software Engineer"
 
   }
+];
+export const navLinks = [
+  {
+    id: "/",
+    title: "Home",
+    icon: <FaHome />,
+  },
+  {
+    id: "/media",
+    title: "Media",
+    icon: <FaFileInvoice />,
+  },
+  {
+    id: "/message",
+    title: "Message",
+    icon: <FaEnvelope />,
+  },
+  
+  
 ];
