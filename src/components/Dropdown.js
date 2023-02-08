046bdb0c0e4506f413bc328237321f2e4ba05d09
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-08 16:01:17 +0600

    added user dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+export default function Dropdown({ setUserOpen, userOpen, user}) {
+    const {name, email, photoURL,} = user;
+  return (
+    <div className="flex items-center md:order-2">
+      <div className={`absolute top-20  ${userOpen ? "translate-x-[500px]" : "translate-x-[-50px]"} z-50  my-4 text-base list-none  divide-y divide-gray-100 glassmorphism rounded-lg shadow  dark:divide-gray-600 transition-all duration-500 ease-in`} id="user-dropdown">
+        <div className="px-4 py-3">
+          <span className="block text-sm text-gray-900 dark:text-white">{name}</span>
+          <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">{email}</span>
+        </div>
+        <ul className="py-2" aria-labelledby="user-menu-button">
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+          </li>
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+          </li>
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+          </li>
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+          </li>
+        </ul>
+      </div>
+      <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button>
+  </div>
+  )
+}
