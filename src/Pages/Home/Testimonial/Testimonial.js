dc57a914a1ffaae33088fe8a84e50a6758958e65
xbayazid <bayazid9006@gmail.com>
2023-02-09 21:59:21 +0600

    hero section redesigned

---
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Testimonial = ({review}) => {
+    const {id, name, description} = review;
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Testimonial;
