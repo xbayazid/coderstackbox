c30911ee93e3af9d034ac3d0d5cd503a8a70d918
0178811jes <jesminara8653@gmail.com>
2023-02-09 13:24:57 +0600

    card

--- src/Pages/Dashboard/AdminPannel/Profile/ProfileCard.js
+++ src/Pages/Dashboard/AdminPannel/Profile/ProfileCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProfileCard = () => {
     return (
         <div className='text-2xl'>
-            card..... COMMING SOON....
+            card..... COMMING SOON.....
         </div>
     );
 };
