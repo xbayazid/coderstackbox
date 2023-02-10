5f3896564886c88fc4bb8defcbc2a41f80ca55e4
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-10 17:17:19 +0600

    service sec animation

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -23,8 +23,10 @@ export const slideIn = (direction, type, delay, duration) => ({
   hidden: {
     x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
     y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
+    opacity: 0
   },
   show: {
+    opacity: 1,
     x: 0,
     y: 0,
     transition: {
