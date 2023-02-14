0e97b42374224e769d33df19ceb7ca2cfe72aea8
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-02-14 03:29:42 -0800

    rebuild upload image popup

--- src/components/ImageUpdateModal/ImageUpdateModal.js
+++ src/components/ImageUpdateModal/ImageUpdateModal.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FaTimes, FaUpload } from 'react-icons/fa';
+import { FaCloudUploadAlt, FaImage, FaTimes, FaUpload, FaUserAlt } from 'react-icons/fa';
 import CropEasy from '../Crop/CropEasy';
 import ProfileModal from '../Crop/ProfileModal/ProfileModal';
+import Button from '../Buttons/Button';
 
 const ImageUpdateModal = ({isVisible, onClose}) => {
 
@@ -14,8 +15,17 @@ const ImageUpdateModal = ({isVisible, onClose}) => {
          justify-center items-center '>
             <div className='flex items-end flex-col'>
                 <FaTimes onClick={ () => onClose()} className='text-white text-2xl' />
-                <div className='md:w-[600px] w-[90vw] h-[80vh] md:h-[500px] bg-slate-600 p-2 rounded-md'>
-                    <h2>Modal</h2>
+                <div className='md:w-[600px] w-[90vw] p-10 h-[80vh] md:h-[500px] flex flex-col justify-between items-center bg-slate-600 rounded-md'>
+                    <h2 className='text-2xl text-center'>Uplod your photo</h2>
+                    <div className='flex flex-col w-36 h-36 rounded-full  border-green-600 justify-center items-center'>
+                        <label htmlFor='userImg' className='my-10 text-center text-2xl'>
+                            {/* <FaUserAlt className=''></FaUserAlt> */}
+                            <FaCloudUploadAlt className='text-9xl'></FaCloudUploadAlt>
+                        </label>
+                    </div>
+                    <input className='hidden' id='userImg' type='file' />
+
+                    <Button className='flex bottom-0 gap-5 justify-center items-center'>Save</Button>
                     
                 </div>
             </div>
