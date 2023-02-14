db073a2a19c7a242db14f77be7e974af2c83042e
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-15 02:21:50 +0600

    added compiled code editor

---
+++ src/Pages/CodeEditor/OnlineIDE/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CodeCell from "./component/code-cell";
+import TextEditor from "./component/text-editor";
+
+const IDE = () => {
+  return (
+    <>
+    
+      <CodeCell />
+    </>
+  );
+};
+
+export default IDE;
