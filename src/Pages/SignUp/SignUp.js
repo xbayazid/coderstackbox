cabe4f1c45f7613381e693b22bae19af3abf2a08
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-02-16 07:16:17 -0800

    remove unnessery code from signup page and added image upload system

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -70,37 +70,6 @@ const SingUp = () => {
       });
   };
 
-
-  /*   const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const name = form.name.value;
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(name, email, password);
-
-    const userInfo = {
-      displayName: name,
-    };
-
-    createUser(email, password)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
-        toast("User Created Successfully");
-
-        updateUser(userInfo)
-          .then(() => {
-          })
-          .catch((error) => console.log(error));
-        form.reset();
-        setError("");
-      })
-      .catch((error) => {
-        console.error(error);
-        setError(error.message);
-      });
-  }; */
   const handleSignUp = (data) => {
     setLoading(true);
     setError("");
@@ -155,49 +124,6 @@ const SingUp = () => {
         <title>CodersStackBox - SignUp</title>
       </Helmet>
       <Navbar></Navbar>
-      {/* <div className='grid gap-4 md:grid-cols-1 lg:grid-cols-2'>
-        <div className={`${layout.sectionCol}`}>
-          <img className='extra-img w-4/5' src="https://i.ibb.co/S3ScgR8/118046-lf20-oahmox5rjson.gif" alt="" />
-        </div>
-        <div className='extra p-2'>
-          <div className='text-white text-center xl:mt-16'>
-            <div className='flex justify-around w-56 mx-auto'>
-              <div>
-                <h3 className='uppercase mb-3 -ml-10 authentication-title'>Signup</h3>
-              </div>
-              <div>
-                <Link to='/login'>
-                  <button className="btn btn-xs ml-10">Login</button>
-                </Link>
-              </div>
-
-            </div>
-            <div className='mt-6'>
-              <form onSubmit={handleSubmit(handleSignUp)}>
-                <div className='mb-5'>
-                  <input type="name" name='name' placeholder="Full Name" className="input input-bordered w-full max-w-xs text-black" required />
-                </div>
-                <div>
-                  <input type="email" name='email' placeholder="Email Address" className="input input-bordered w-full max-w-xs text-black" required />
-                </div>
-                <div className='my-5'>
-                  <input type="password" name='password' placeholder="Password" className="input input-bordered w-full max-w-xs text-black" required />
-                </div>
-                <div>
-                  <button className='bg-black px-32 py-3 text-white rounded-md' type='submit'>Register</button>
-                </div>
-                <div className="divider w-52 mx-auto">OR</div>
-                <div className='mx-auto p-3 rounded-md bg-gradient-to-r from-accent to-secondary' style={{ width: '317px' }}>
-                  <button onClick={handleGoogleSignIn}>Register with Google</button>
-                </div>
-                <div className='mx-auto p-3 rounded-md mt-4 bg-gradient-to-r from-accent to-secondary' style={{ width: '317px' }}>
-                  <button onClick={hangleGithubSingIn}>Register with GitHub</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <div className="flex justify-center mt-24">
         <div className="glassmorphism py-12 px-8 rounded-tl-xl rounded-bl-xl">
           <h2 className="mt-12 text-white text-5xl title">Welcome to</h2>
