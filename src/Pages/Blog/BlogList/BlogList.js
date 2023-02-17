5e8b739c0c9d74f35c74901747c73bcfb5d58224
xbayazid <bayazid9006@gmail.com>
2023-02-17 17:57:18 +0600

    conflict clearing

--- src/Pages/Blog/BlogList/BlogList.js
+++ src/Pages/Blog/BlogList/BlogList.js
@@ -4,7 +4,7 @@ import './BlogList.css';
 
 const BlogList = ({blogs}) => {
     return (
-        <div className="blogList-wrap">
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {blogs.map((blog) => (
                 <BlogItem blog={blog} key={blog.id}/>
             ))}
