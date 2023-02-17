f85c1a1e2a4a6a113a9cdf63ad981e548c7d87f2
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-17 15:59:50 +0600

    logo size responsived

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -30,7 +30,7 @@ const Sidebar = ({ isOpen, desktopSidebarRef, setIsOpen, user }) => {
         } flex h-screen w-full flex-col justify-between border-r bg-white px-6 pb-3 transition-all duration-700 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%] dark:bg-gray-800 dark:border-gray-700`}
       >
         <div>
-          <div className="-mx-6 px-6 py-4 flex justify-between items-center">
+          <div className="-mx-8 px-6 py-4 flex justify-between items-center">
             <Logo styles="w-32" />
             <div
               onClick={() => setIsOpen(false)}
