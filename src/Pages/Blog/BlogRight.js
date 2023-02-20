f84ebc97fd4420eb51bc5139fb6b5c266d0f97be
0178811jes <jesminara8653@gmail.com>
2023-02-20 18:14:09 +0600

    blog right

--- src/Pages/Blog/BlogRight.js
+++ src/Pages/Blog/BlogRight.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+
+
 const BlogRight = () => {
+
+    
+
     return (
-        <div className='w-64 blogRight text-xl text-white pl-10'>
-            Blog Right
+        <div className='w-64 blogRight text-xl text-white pl-10 mt-10'>
+            <h1  className=''>Category</h1>
+           <div className='mt-10'>
+           <a  className='mt-10' href="#development">Development</a> <br />
+           </div>
+
         </div>
     );
 };
