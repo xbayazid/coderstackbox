6953aab70c5c79b2de3ddf07769f3d54c644f002
0178811jes <jesminara8653@gmail.com>
2023-02-22 18:46:16 +0600

    blog right side done

--- src/Pages/Blog/BlogRight.js
+++ src/Pages/Blog/BlogRight.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { blogRightSide } from '../../constants';
+import SearchBar from './SearchBar/SearchBar';
 
 
 
@@ -7,11 +9,18 @@ const BlogRight = () => {
     
 
     return (
-        <div className='w-64 blogRight text-xl text-white pl-10 mt-10'>
-            <h1  className=''>Category</h1>
-           <div className='mt-10'>
-           <a  className='mt-10' href="#development">Development</a> <br />
-           </div>
+        <div className='lg:block hidden w-64 blogRight text-xl text-white pl-10 mt-10'>
+            <h1  className='text-2xl font-bold mb-5'>Category</h1>
+           
+           <ul className='mt-5'>
+                
+                   {
+                    blogRightSide.map(m => <li className='text-xl text-white mb-10' key={m.id}><a href={m.href}>{m.name}</a></li>)
+                   }
+
+                   <SearchBar/>
+
+                </ul>
 
         </div>
     );
