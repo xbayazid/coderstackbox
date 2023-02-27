2df59c579b3b116d820ebc4fa9c3a3902b950c41
Tamjid-Mostafa <tamjid430@gmail.com>
2023-02-27 15:55:29 +0600

    my collection

--- src/components/Modals/LoginModal.js
+++ src/components/Modals/LoginModal.js
@@ -4,37 +4,30 @@ import { FaGoogle } from "react-icons/fa";
 import { setAuthToken } from "../../api/auth";
 import { logo } from "../../assets";
 import { AuthContext } from "../../context/AuthProvider";
+import { TitleText } from "../CustomText/CustomText";
 import LoadingDots from "../LoadingDots/LoadingDots";
+import Logo from "../Logo";
 import Modal from "./ProjectSave";
 
-const LogInModal = ({
-  showLogInModal,
-  setShowLogInModal,
-  
-}) => {
-  const [logInClicked, setLogInClicked] = useState(false);
+const LogInModal = ({ showLogInModal, setShowLogInModal }) => {
   const [error, setError] = useState("");
-console.log()
-const { providerLogin, setLoading, loading } =
-    useContext(AuthContext);
-const googleAuthProvider = new GoogleAuthProvider();
-const handleGoogleSignIn = () => {
-    setLoading(true)
+  const { providerLogin, setLoading, loading } = useContext(AuthContext);
+  const googleAuthProvider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    setLoading(true);
     providerLogin(googleAuthProvider)
       .then((result) => {
-        setLoading(false)
+        setLoading(false);
         const user = result.user;
         if (user.uid) {
           const userInfo = {
             name: user.displayName,
             email: user.email,
-            photoURL: user.photoURL
-            ,
+            photoURL: user.photoURL,
           };
           console.log(result);
           setAuthToken(userInfo);
-          setShowLogInModal(false)
-          
+          setShowLogInModal(false);
         }
       })
       .catch((error) => {
@@ -43,47 +36,47 @@ const handleGoogleSignIn = () => {
   };
 
   return (
-    <Modal
-      showModal={showLogInModal}
-      setShowModal={setShowLogInModal}
-    >
+    <Modal showModal={showLogInModal} setShowModal={setShowLogInModal}>
       <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="/">
-            <img
-              src={logo}
-              alt="Logo"
-              className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
-            />
-          </a>
+          <Logo className="text-primary" />
           <h3 className="font-display text-2xl font-bold">Sign In</h3>
-          <p className="text-sm text-gray-500">
-            This is strictly for demo purposes - only your email and profile
-            picture will be stored.
+          <p className="font-poppins font-semibold text-lg tracking-[0.2em] text-gray-500">
+            {`🍕Eat > 😴Sleep > 👨🏻‍💻 Code > 🔁 Repeat`}
           </p>
         </div>
-
-        <div className="flex flex-col space-y-4 bg-gray-50 px-4 py-8 md:px-16">
-          <button
-            disabled={loading}
-            className={`${
+        {!error ? (
+          <div className="flex flex-col space-y-4 bg-gray-50 px-4 py-8 md:px-16">
+            <button
+              disabled={loading}
+              className={`${
                 loading
-                ? "cursor-not-allowed border-gray-200 bg-gray-100"
-                : "border border-gray-200 bg-white text-black hover:bg-gray-50"
-            } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
-            onClick={handleGoogleSignIn}
-          >
-            { loading ? (
-              <LoadingDots color="#808080" />
-            ) : (
+                  ? "cursor-not-allowed border-gray-200 bg-gray-100"
+                  : "border border-gray-200 bg-white text-black hover:bg-gray-50"
+              } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
+              onClick={handleGoogleSignIn}
+            >
+              {loading ? (
+                <LoadingDots color="#808080" />
+              ) : (
+                <>
+                  <p className="flex items-center gap-4">
+                    <FaGoogle />
+                    {"Sign In"}
+                  </p>
+                </>
+              )}
+            </button>
+          </div>
+        ) : (
+          <TitleText
+            title={
               <>
-                <p className="flex items-center gap-4"><FaGoogle/>{"Sign In"}</p>
+                There is an Error. <br /> Please Try again.
               </>
-            )}
-          </button>
-        </div>
+            }
+          />
+        )}
       </div>
     </Modal>
   );
@@ -99,7 +92,7 @@ export function useLogInModal() {
         setShowLogInModal={setShowLogInModal}
       />
     );
-  }, [showLogInModal, setShowLogInModal,]);
+  }, [showLogInModal, setShowLogInModal]);
 
   return useMemo(
     () => ({
